b9a26f590d24848ad3b8e107bb7d7ae9d649c66e
Jens Pitkänen <jens@neon.moe>
2018-02-15 10:09:59 +0200

    Lisaa kommentteja

--- perusteet/input_ja_output.py
+++ perusteet/input_ja_output.py
@@ -1,10 +1,11 @@
 # Kytke nappi pietsokaiutin pinniin 1 ja gnd-porttiin. Napin voit rakentaa kytkemalla johdon
 # maadoitukseen, ja koskettamalla digiporttia 0.
 
-from microbit import *
+from microbit import * # Kerro Pythonille että käytetään microbitin ominaisuuksia (esim. pin0)
 
-while True:
-    if pin0.is_touched():
-        pin1.write_digital(1)
-    else: 
-        pin1.write_digital(0)
+while True: # Avaa silmukka
+    if pin0.is_touched(): # Jos pin0:n kosketaan...
+        pin1.write_digital(1) # Pistetään pin1:een virta päälle
+    else: # Jos pin0:n ei koskettukaan...
+        pin1.write_digital(0) # PIstetään pin1:n virta pois päältä
+    # Ei enempää suoritettavaa, palaa silmukan alkuun
