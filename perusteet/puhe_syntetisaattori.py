b8575143a2cb6f4f5a2031de7d3921e39e98e593
Jens Pitkänen <jens@neon.moe>
2018-02-16 09:01:58 +0200

    Korjaus syntetisaattoriin

--- perusteet/puhe_syntetisaattori.py
+++ perusteet/puhe_syntetisaattori.py
@@ -4,11 +4,12 @@
 import speech # Kerrotaan Pythonille että käytämme puhumisominaisuuksia (ei sisälly alempaan)
 from microbit import * # Kerrotaan Pythonille että käytämme microbitin sisäänrakennettuja muitakin toimintoja
 
+display.show(Image.HAPPY) # Näytetään hymynaamaa
 while True: # Aloitetaan toistosilmukka
-    display.show(Image.HAPPY) # Näytetään hymynaamaa
-    if button_a.was_pressed(): # Mikäli nappia painettiin...
+    if button_a.is_pressed(): # Mikäli nappia painettiin...
         display.clear() # Tyhjennetään ruutu
         sleep(5000) # Odotetaan 5 sekuntia
         display.show(Image.MUSIC_QUAVER) # Näytetään musikaalinen kuva, viitaten siihen että micro:bit puhuu nyt
         painallukset = button_a.get_presses() + 1 # Painalluksien määrään pitää laskea myös se jolla siirryimme tähän if-lausekkeeseen (+ 1)
         speech.say(str(painallukset)) # micro:bit sanoo luvun, str muuttaa luvun tekstiksi
+        display.show(Image.HAPPY) # Näytetään taas hymynaamaa
