cdecb327b20ca21636d6959f1e67171c26b4532e
Matti Heikkinen <matti.heikkinen@eduespoo.fi>
2018-10-08 18:16:15 +0300

    Add Arduino projects with microbit

---
+++ microbit_with_arduino/MicroBitBlynkPOVDisplay/MicroBitBlynkPOVDisplay.ino
@@ -0,0 +1,188 @@
+/*************************************************************
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Social networks:            http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  This example shows how to use BBC Micro:Bit
+  to connect your project to Blynk.
+
+  Note: This requires nRF5 support package:
+    https://github.com/sandeepmistry/arduino-nRF5
+
+  And BLEPeripheral library
+    from http://librarymanager/all#BLEPeripheral
+    or https://github.com/sandeepmistry/arduino-BLEPeripheral
+
+  1. Select: Tools -> SoftDevice -> S110
+  2. Select: Tools -> Programmer -> CMSIS-DAP
+  3. Select: Tools -> nRF5 Flash SoftDevice
+  4. Read and Accept License
+  5. Verify and Upload Sketch
+
+  NOTE: BLE support is in beta!
+
+ *************************************************************/
+
+#define BLYNK_USE_DIRECT_CONNECT
+
+#define BLYNK_PRINT Serial
+
+#include <BlynkSimpleSerialBLE.h>
+#include <BLEPeripheral.h>
+#include "BLESerial.h"
+#include <Adafruit_Microbit.h>
+
+Adafruit_Microbit_Matrix microbit;
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "Your secret token from Blynk app"; 
+
+
+// Create ble serial instance, parameters are ignored for MicroBit
+BLESerial SerialBLE(0, 0, 0);
+
+
+char display_char[] = "hello ";   // Don't use capitals.  
+
+int delayTime = 2;  // try 1 ms
+int charBreak = 4;  // try 2 ms
+
+
+
+void setup() {
+  Serial.begin(9600);
+
+  SerialBLE.setLocalName("Blynk");
+  SerialBLE.setDeviceName("Blynk");
+  SerialBLE.setAppearance(0x0080);
+  SerialBLE.begin();
+
+  Blynk.begin(SerialBLE, auth);
+
+  Serial.println("Waiting for connections...");
+  microbit.begin();
+  microbit.clear();
+
+  
+}
+
+// In Blynk connect terminal widget to virtual port V2.
+BLYNK_WRITE(V2){
+
+String str = param.asStr();
+
+
+//if ( str.length() > 0){
+// Length (with one extra character for the null terminator)
+int str_len =str.length()+1;
+// Prepare the character array (the buffer)
+char char_array[str_len];
+// copy string to char
+str.toCharArray(display_char,str_len);
+
+}
+
+
+int a[] = {1, 6, 26, 6, 1};
+int b[] = {31, 21, 21, 10, 0};
+int c2[] = {14, 17, 17, 10, 0};
+int d[] = {31, 17, 17, 14, 0};
+int e[] = {31, 21, 21, 17, 0};
+int f[] = {31, 20, 20, 16, 0};
+int g[] = {14, 17, 19, 10, 0};
+int h[] = {31, 4, 4, 4, 31};
+int i[] = {0, 17, 31, 17, 0};
+int j[] = {0, 17, 30, 16, 0};
+int k[] = {31, 4, 10, 17, 0};
+int l[] = {31, 1, 1, 1, 0};
+int m[] = {31, 12, 3, 12, 31};
+int n[] = {31, 12, 3, 31, 0};
+int o[] = {14, 17, 17, 14, 0};
+int p[] = {31, 20, 20, 8, 0};
+int q[] = {14, 17, 19, 14, 2};
+int r[] = {31, 20, 22, 9, 0};
+int s[] = {8, 21, 21, 2, 0};
+int t[] = {16, 16, 31, 16, 16};
+int u[] = {30, 1, 1, 30, 0};
+int v[] = {24, 6, 1, 6, 24};
+int w[] = {28, 3, 12, 3, 28};
+int x[] = {17, 10, 4, 10, 17};
+int y[] = {17, 10, 4, 8, 16};
+int z[] = {19, 21, 21, 25, 0};
+
+int eos[] = {0, 1, 0, 0, 0};
+int excl[] = {0, 29, 0, 0, 0};
+int ques[] = {8, 19, 20, 8, 0};
+
+void displayLine(int line){
+int myline;
+myline = line;
+if (myline>=16) {microbit.drawPixel(2, 0, LED_ON); myline-=16;} else {microbit.drawPixel(2, 0, LED_OFF);}
+if (myline>=8)  {microbit.drawPixel(2, 1, LED_ON); myline-=8;}  else {microbit.drawPixel(2, 1, LED_OFF);}
+if (myline>=4)  {microbit.drawPixel(2, 2, LED_ON); myline-=4;}  else {microbit.drawPixel(2, 2, LED_OFF);}
+if (myline>=2)  {microbit.drawPixel(2, 3, LED_ON); myline-=2;}  else {microbit.drawPixel(2, 3, LED_OFF);}
+if (myline>=1)  {microbit.drawPixel(2, 4, LED_ON); myline-=1;}  else {microbit.drawPixel(2, 4, LED_OFF);}
+}
+
+void displayChar(char c){
+if (c == 'a'){for (int i = 0; i <5; i++){displayLine(a[i]);delay(delayTime);}displayLine(0);}
+if (c == 'b'){for (int i = 0; i <5; i++){displayLine(b[i]);delay(delayTime);}displayLine(0);}
+if (c == 'c'){for (int i = 0; i <5; i++){displayLine(c2[i]);delay(delayTime);}displayLine(0);}
+if (c == 'd'){for (int i = 0; i <5; i++){displayLine(d[i]);delay(delayTime);}displayLine(0);}
+if (c == 'e'){for (int i = 0; i <5; i++){displayLine(e[i]);delay(delayTime);}displayLine(0);}
+if (c == 'f'){for (int i = 0; i <5; i++){displayLine(f[i]);delay(delayTime);}displayLine(0);}
+if (c == 'g'){for (int i = 0; i <5; i++){displayLine(g[i]);delay(delayTime);}displayLine(0);}
+if (c == 'h'){for (int i = 0; i <5; i++){displayLine(h[i]);delay(delayTime);}displayLine(0);}
+if (c == 'i'){for (int it = 0; it <5; it++){displayLine(i[it]);delay(delayTime);}displayLine(0);}
+if (c == 'j'){for (int i = 0; i <5; i++){displayLine(j[i]);delay(delayTime);}displayLine(0);}
+if (c == 'k'){for (int i = 0; i <5; i++){displayLine(k[i]);delay(delayTime);}displayLine(0);}
+if (c == 'l'){for (int i = 0; i <5; i++){displayLine(l[i]);delay(delayTime);}displayLine(0);}
+if (c == 'm'){for (int i = 0; i <5; i++){displayLine(m[i]);delay(delayTime);}displayLine(0);}
+if (c == 'n'){for (int i = 0; i <5; i++){displayLine(n[i]);delay(delayTime);}displayLine(0);}
+if (c == 'o'){for (int i = 0; i <5; i++){displayLine(o[i]);delay(delayTime);}displayLine(0);}
+if (c == 'p'){for (int i = 0; i <5; i++){displayLine(p[i]);delay(delayTime);}displayLine(0);}
+if (c == 'q'){for (int i = 0; i <5; i++){displayLine(q[i]);delay(delayTime);}displayLine(0);}
+if (c == 'r'){for (int i = 0; i <5; i++){displayLine(r[i]);delay(delayTime);}displayLine(0);}
+if (c == 's'){for (int i = 0; i <5; i++){displayLine(s[i]);delay(delayTime);}displayLine(0);}
+if (c == 't'){for (int i = 0; i <5; i++){displayLine(t[i]);delay(delayTime);}displayLine(0);}
+if (c == 'u'){for (int i = 0; i <5; i++){displayLine(u[i]);delay(delayTime);}displayLine(0);}
+if (c == 'v'){for (int i = 0; i <5; i++){displayLine(v[i]);delay(delayTime);}displayLine(0);}
+if (c == 'w'){for (int i = 0; i <5; i++){displayLine(w[i]);delay(delayTime);}displayLine(0);}
+if (c == 'x'){for (int i = 0; i <5; i++){displayLine(x[i]);delay(delayTime);}displayLine(0);}
+if (c == 'y'){for (int i = 0; i <5; i++){displayLine(y[i]);delay(delayTime);}displayLine(0);}
+if (c == 'z'){for (int i = 0; i <5; i++){displayLine(z[i]);delay(delayTime);}displayLine(0);}
+if (c == '!'){for (int i = 0; i <5; i++){displayLine(excl[i]);delay(delayTime);}displayLine(0);}
+if (c == '?'){for (int i = 0; i <5; i++){displayLine(ques[i]);delay(delayTime);}displayLine(0);}
+if (c == '.'){for (int i = 0; i <5; i++){displayLine(eos[i]);delay(delayTime);}displayLine(0);}
+delay(charBreak);
+}
+
+void displayString(char* s){
+for (int i = 0; i<=strlen(s); i++){
+displayChar(s[i]);
+}
+}
+
+
+void loop() {
+  SerialBLE.poll();
+
+  if (SerialBLE) {    // If BLE is connected...
+    displayString(display_char);
+    Blynk.run();
+  }
+}
+
